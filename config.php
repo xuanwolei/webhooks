482fd8a7d0d8c052a8073192fddd7de8f2cb28cd
ybc <429710096@qq.com>
2018-12-05 11:41:20 +0800

    add config

--- config.php
+++ config.php
@@ -2,7 +2,8 @@
 return array(
 	'key' => '9a218c9857a815bdfcade5b5124f72e2',  //秘钥
 	'projects' => array( //hooks项目列表
-		'SwooleDistributed' => '/home/wwwroot/default/SwooleDistributed'
+		'SwooleDistributed' => '/home/wwwroot/default/SwooleDistributed',
+		'go-demo' 			=> '/home/wwwroot/go',
 	),
 	'ip_white_on' => false, //关闭ip白名单验证
 	'ip_white_list' => array(
